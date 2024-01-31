fa9a188913dcdd16bfb165aefe378b938578df1e
Dante Castelao <34772417+DanteCastelao@users.noreply.github.com>
2024-01-31 11:10:57 -0300

    Update layout.js

--- front-end/app/layout.js
+++ front-end/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Analytics Tracking Code */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WRD1CS854D"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WRD1CS854D');
+            `,
+          }}
+        ></script>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
