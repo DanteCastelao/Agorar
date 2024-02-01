4e0ea7d0597b18d092e9d4f78ce142830bc70893
Dante Castelao <34772417+DanteCastelao@users.noreply.github.com>
2024-01-31 21:37:38 -0300

    Analytics

--- front-end/app/layout.js
+++ front-end/app/layout.js
@@ -1,6 +1,6 @@
-import { Inter, Vidaloka } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,26 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/Icon.png" type="image/png"/>
+      <link rel="icon" href="/Icon.png" type="image/png"/>
       </head>
-      <Head>
-        {/* Google Analytics Tracking Code */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-WRD1CS854D"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-13RT3WTLD9');
-            `,
-          }}
-        ></script>
-      </Head>
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-13RT3WTLD9" />
     </html>
   );
 }
