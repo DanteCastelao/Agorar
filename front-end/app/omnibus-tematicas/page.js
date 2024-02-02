4a8accf0f38eee416b1777c454fcbceae71c069f
Dante Castelao <34772417+DanteCastelao@users.noreply.github.com>
2024-02-02 12:36:48 -0300

    Change Title

--- front-end/app/omnibus-tematicas/page.js
+++ front-end/app/omnibus-tematicas/page.js
@@ -1,15 +1,12 @@
 "use client";
-import React, { useEffect, useState, useRef } from "react";
-import Article from "../components/article";
-import Chart from "chart.js/auto";
-import { Vidaloka } from "next/font/google";
-import WhatsappIcon from "../assets/WhatsAppIcon.png"
-import TwitterIcon from "../assets/TwitterIcon.png"
-import FacebookIcon from "../assets/FacebookIcon.png"
+import React, {  } from "react";
 import Themes from "../components/themes";
 
 export default function Home() {
-
+  useEffect(() => {
+		document.title = "Agorar | Omnibus";
+	  }, []);
+    
   return (
     <main className="flex min-h-screen flex-col bg-[#F3F6F9]">
       <Themes category="omnibus" />
--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,11 +1,15 @@
 "use client";
-import React, {  } from "react";
+import React, { useEffect } from "react";
 import Themes from "./components/themes";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col bg-[#F3F6F9]">
-      <Themes category="dnu" />
-    </main>
-  );
+	useEffect(() => {
+		document.title = "Agorar | DNU";
+	  }, []);
+
+	return (
+		<main className="flex min-h-screen flex-col bg-[#F3F6F9]">
+		<Themes category="dnu" />
+		</main>
+	);
 }
