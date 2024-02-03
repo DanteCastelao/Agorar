854af3dc3bc926d95f19bf3e317ccfacc6e687dc
Tomas Caula <tomasbenjamincaula@gmail.com>
2024-02-03 19:38:13 -0300

    Fix Footer

--- front-end/app/layout.js
+++ front-end/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         </head>
         <body className={inter.className}>
             {children}
+            <Footer/>
         </body>
         <GoogleAnalytics gaId="G-13RT3WTLD9" />
       </html>
